0010f320e8bd33ed6251df0845aa1ed64213ca97
Nick <blizzard@gmail.com>
2015-11-28 17:07:58 -0500

    Initial commit

---
+++ Engine.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Chip8
+{
+    internal class Engine
+    {
+        private const uint MEMORY_SIZE = 0x1000;
+        private const byte REGISTER_COUNT = 0x10;
+        private const byte STACK_SIZE = 0x10;
+        private const byte SCREEN_WIDTH = 64;
+        private const byte SCREEN_HEIGHT = 32;
+        private const byte BITS_IN_BYTE = 8;
+        private const uint MEMORY_ROM_OFFSET = 0x200;
+
+        private byte[] _memory;
+        private byte[] _v; // registers
+        private uint _i; // index register
+        private uint _pc; // program counter
+        private uint[] _stack;
+        private byte _sp; // stack pointer
+        private byte[] _screen;
+        private byte _delayTimer;
+        private byte _soundTimer;
+
+        private Dictionary<byte, Action<uint>> _opcodeMap;
+
+        internal Engine()
+        {
+            _memory = new byte[MEMORY_SIZE];
+            _v = new byte[REGISTER_COUNT];
+            _stack = new uint[STACK_SIZE];
+            _screen = new byte[(SCREEN_WIDTH / BITS_IN_BYTE) * (SCREEN_HEIGHT / BITS_IN_BYTE)];
+
+            byte[] rom = File.ReadAllBytes("game.ch8");
+            Buffer.BlockCopy(rom, 0, _memory, (int)MEMORY_ROM_OFFSET, rom.Length);
+
+            _i = 0;
+            _pc = MEMORY_ROM_OFFSET;
+            _sp = 0;
+            _delayTimer = 0;
+            _soundTimer = 0;
+
+            _opcodeMap = new Dictionary<byte, Action<uint>>();
+            _opcodeMap.Add(0x6, loadRegister);
+            _opcodeMap.Add(0xA, loadIRegister);
+        }
+
+        internal void Clock()
+        {
+            uint opcode = (uint)(_memory[_pc] << 8) | _memory[_pc + 1];
+
+            byte opcodeMSN = (byte)((opcode & 0xF000) >> 12);
+            _opcodeMap[opcodeMSN](opcode);
+
+            _pc += 2;
+        }
+
+        #region Operations
+
+        // 6XNN
+        private void loadRegister(uint opcode)
+        {
+            byte register = (byte)((opcode & 0x0F00) >> 8);
+            byte value = (byte)(opcode & 0x00FF);
+            _v[register] = value;
+        }
+
+        // ANNN
+        private void loadIRegister(uint opcode)
+        {
+            uint address = opcode & 0x0FFF;
+            _i = address;
+        }
+
+        #endregion
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using SDL2;
+using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace Chip8
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Engine engine = new Engine();
+            engine.Clock();
+            engine.Clock();
+            engine.Clock();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            IntPtr mainWindowHandle = new WindowInteropHelper(Application.Current.MainWindow).Handle;
+
+            SDL.SDL_Rect drawableRegion = new SDL.SDL_Rect()
+            {
+                x = (int)drawableSurface.TransformToAncestor(Application.Current.MainWindow).Transform(new Point(0, 0)).X,
+                y = (int)drawableSurface.TransformToAncestor(Application.Current.MainWindow).Transform(new Point(0, 0)).Y,
+                w = (int)drawableSurface.Width,
+                h = (int)drawableSurface.Height
+            };
+            SDL.SDL_Rect drawableSize = new SDL.SDL_Rect() { w = drawableRegion.w, h = drawableRegion.h };
+
+            SDL.SDL_Init(SDL.SDL_INIT_EVERYTHING);
+
+            //IntPtr sdlWindowPointer = SDL.SDL_CreateWindow("My Game Window", SDL.SDL_WINDOWPOS_UNDEFINED, SDL.SDL_WINDOWPOS_UNDEFINED, 256, 128, SDL.SDL_WindowFlags.SDL_WINDOW_OPENGL);
+            IntPtr sdlWindowPointer = SDL.SDL_CreateWindowFrom(mainWindowHandle);
+
+            IntPtr sdlRendererPointer = SDL.SDL_CreateRenderer(sdlWindowPointer, -1, 0);
+            SDL.SDL_SetRenderDrawColor(sdlRendererPointer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+            IntPtr sdlSurfacePointer = SDL.SDL_CreateRGBSurface(0, drawableSize.w, drawableSize.h, 32, 0x00FF0000, 0x0000FF00, 0x000000FF, 0xFF000000);
+            SDL.SDL_Surface sdlSurface = (SDL.SDL_Surface)Marshal.PtrToStructure(sdlSurfacePointer, typeof(SDL.SDL_Surface));
+
+            IntPtr sdlTexturePointer = SDL.SDL_CreateTexture(sdlRendererPointer, SDL.SDL_PIXELFORMAT_ARGB8888, (int)SDL.SDL_TextureAccess.SDL_TEXTUREACCESS_STREAMING, drawableSize.w, drawableSize.h);
+
+            SDL.SDL_FillRect(sdlSurfacePointer, ref drawableSize, SDL.SDL_MapRGB(sdlSurface.format, 0xFF, 0x00, 0x00));
+
+            SDL.SDL_UpdateTexture(sdlTexturePointer, IntPtr.Zero, sdlSurface.pixels, sdlSurface.pitch);
+
+            SDL.SDL_RenderClear(sdlRendererPointer);
+            SDL.SDL_RenderCopy(sdlRendererPointer, sdlTexturePointer, IntPtr.Zero, ref drawableRegion);
+            SDL.SDL_RenderPresent(sdlRendererPointer);
+        }
+    }
+}
