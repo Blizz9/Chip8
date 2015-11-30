d55d2666229131091df19175664ce3b0079b8a1b
Nick <blizzard@gmail.com>
2015-11-30 18:27:39 -0500

    Many many changes; moved to OpenTK and got a really good version working

--- Engine.cs
+++ Engine.cs
@@ -1,25 +1,26 @@
-﻿using SDL2;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Runtime.InteropServices;
 using System.Threading;
 
 namespace Chip8
 {
     // TODO: Make screen storage bits instead of bytes
+    // TODO: Check if tight loops take up all of the CPU
 
     internal class Engine
     {
+        public const byte SCREEN_WIDTH = 64;
+        public const byte SCREEN_HEIGHT = 32;
+
         private const uint MEMORY_SIZE = 0x1000;
         private const byte REGISTER_COUNT = 0x10;
         private const byte STACK_SIZE = 0x10;
-        private const byte SCREEN_WIDTH = 64;
-        private const byte SCREEN_HEIGHT = 32;
         private const byte BITS_IN_BYTE = 8;
         private const uint MEMORY_ROM_OFFSET = 0x200;
-        private const byte OPCODE_SIZE = 2;
+        private const byte OPCODE_SIZE = 0x2;
+        private const byte FONT_SIZE = 0x5;
 
         private byte[] _memory;
         private byte[] _v; // registers
@@ -31,47 +32,198 @@ namespace Chip8
         private byte _delayTimer;
         private byte _soundTimer;
 
+        private List<byte[]> _fonts;
+
         private Dictionary<byte, Action<uint>> _opcodeMap;
+        private Dictionary<byte, Action<uint>> _opcodeMap00EX;
         private Dictionary<byte, Action<uint>> _opcodeMap8XXX;
         private Dictionary<byte, Action<uint>> _opcodeMapEXXX;
         private Dictionary<byte, Action<uint>> _opcodeMapFXXX;
 
-        private object _sync = new object();
+        private readonly object _sync = new object();
+
         private bool _running;
         private bool _paused;
 
-        public Action<byte[]> NewDrawing;
+        private Action _screenRefreshed;
+        private Func<byte> _getKeypress;
 
         internal Engine()
         {
             _memory = new byte[MEMORY_SIZE];
             _v = new byte[REGISTER_COUNT];
             _stack = new uint[STACK_SIZE];
-            //_screen = new byte[(SCREEN_WIDTH / BITS_IN_BYTE) * (SCREEN_HEIGHT / BITS_IN_BYTE)];
-            _screen = new byte[SCREEN_WIDTH * SCREEN_HEIGHT];
+            //Screen = new byte[(SCREEN_WIDTH / BITS_IN_BYTE) * (SCREEN_HEIGHT / BITS_IN_BYTE)];
+            Screen = new byte[SCREEN_WIDTH * SCREEN_HEIGHT];
+
+            _fonts = new List<byte[]>();
+
+            _fonts.Add(new byte[FONT_SIZE]
+            {
+                0xF0, // 1111
+                0x90, // 1001
+                0x90, // 1001
+                0x90, // 1001
+                0xF0  // 1111
+            }); // 0
+            _fonts.Add(new byte[FONT_SIZE]
+            {
+                0x20, // 0010
+                0x60, // 0110
+                0x20, // 0010
+                0x20, // 0010
+                0x70  // 0111
+            }); // 1
+            _fonts.Add(new byte[FONT_SIZE]
+            {
+                0xF0, // 1111
+                0x10, // 0001
+                0xF0, // 1111
+                0x80, // 1000
+                0xF0  // 1111
+            }); // 2
+            _fonts.Add(new byte[FONT_SIZE]
+            {
+                0xF0, // 1111
+                0x10, // 0001
+                0xF0, // 1111
+                0x10, // 0001
+                0xF0  // 1111
+            }); // 3
+            _fonts.Add(new byte[FONT_SIZE]
+            {
+                0x90, // 1001
+                0x90, // 1001
+                0xF0, // 1111
+                0x10, // 0001
+                0x10  // 0001
+            }); // 4
+            _fonts.Add(new byte[FONT_SIZE]
+            {
+                0xF0, // 1111
+                0x80, // 1000
+                0xF0, // 1111
+                0x10, // 0001
+                0xF0  // 1111
+            }); // 5
+            _fonts.Add(new byte[FONT_SIZE]
+            {
+                0xF0, // 1111
+                0x80, // 1000
+                0xF0, // 1111
+                0x90, // 1001
+                0xF0  // 1111
+            }); // 6
+            _fonts.Add(new byte[FONT_SIZE]
+            {
+                0xF0, // 1111
+                0x10, // 0001
+                0x20, // 0010
+                0x40, // 0100
+                0x40  // 0100
+            }); // 7
+            _fonts.Add(new byte[FONT_SIZE]
+            {
+                0xF0, // 1111
+                0x90, // 1001
+                0xF0, // 1111
+                0x90, // 1001
+                0xF0  // 1111
+            }); // 8
+            _fonts.Add(new byte[FONT_SIZE]
+            {
+                0xF0, // 1111
+                0x90, // 1001
+                0xF0, // 1111
+                0x10, // 0001
+                0xF0  // 1111
+            }); // 9
+            _fonts.Add(new byte[FONT_SIZE]
+            {
+                0xF0, // 1111
+                0x90, // 1001
+                0xF0, // 1111
+                0x90, // 1001
+                0x90  // 1001
+            }); // A
+            _fonts.Add(new byte[FONT_SIZE]
+            {
+                0xE0, // 1110
+                0x90, // 1001
+                0xE0, // 1110
+                0x90, // 1001
+                0xE0  // 1110
+            }); // B
+            _fonts.Add(new byte[FONT_SIZE]
+            {
+                0xF0, // 1111
+                0x80, // 1000
+                0x80, // 1000
+                0x80, // 1000
+                0xF0  // 1111
+            }); // C
+            _fonts.Add(new byte[FONT_SIZE]
+            {
+                0xE0, // 1110
+                0x90, // 1001
+                0x90, // 1001
+                0x90, // 1001
+                0xE0  // 1110
+            }); // D
+            _fonts.Add(new byte[FONT_SIZE]
+            {
+                0xF0, // 1111
+                0x80, // 1000
+                0xF0, // 1111
+                0x80, // 1000
+                0xF0  // 1111
+            }); // E
+            _fonts.Add(new byte[FONT_SIZE]
+            {
+                0xF0, // 1111
+                0x80, // 1000
+                0xF0, // 1111
+                0x80, // 1000
+                0x80  // 1000
+            }); // F
+
+            foreach (byte[] font in _fonts)
+                Buffer.BlockCopy(font, 0, _memory, (0x0000 + (_fonts.IndexOf(font) * FONT_SIZE)), FONT_SIZE);
 
             _opcodeMap = new Dictionary<byte, Action<uint>>();
+            _opcodeMap.Add(0x0, map00EXOperations);
             _opcodeMap.Add(0x1, jump);
+            _opcodeMap.Add(0x2, callSubroutine);
             _opcodeMap.Add(0x3, jumpIfEqual);
             _opcodeMap.Add(0x4, jumpIfNotEqual);
             _opcodeMap.Add(0x6, loadRegister);
             _opcodeMap.Add(0x7, addValueToRegister);
             _opcodeMap.Add(0x8, map8XXXOperations);
-            _opcodeMap.Add(0xA, loadIRegister);
+            _opcodeMap.Add(0xA, loadIndex);
             _opcodeMap.Add(0xC, randomAND);
             _opcodeMap.Add(0xD, drawSprite);
             _opcodeMap.Add(0xE, mapEXXXOperations);
             _opcodeMap.Add(0xF, mapFXXXOperations);
+
+            _opcodeMap00EX = new Dictionary<byte, Action<uint>>();
+            _opcodeMap00EX.Add(0xE0, clearScreen);
+            _opcodeMap00EX.Add(0xEE, returnSubroutine);
 
             _opcodeMap8XXX = new Dictionary<byte, Action<uint>>();
             _opcodeMap8XXX.Add(0x2, andRegisters);
             _opcodeMap8XXX.Add(0x4, addRegisters);
 
             _opcodeMapEXXX = new Dictionary<byte, Action<uint>>();
+            _opcodeMapEXXX.Add(0x9E, jumpIfKeyPressed);
             _opcodeMapEXXX.Add(0xA1, jumpIfKeyNotPressed);
 
             _opcodeMapFXXX = new Dictionary<byte, Action<uint>>();
             _opcodeMapFXXX.Add(0x0A, waitForKeypress);
+            _opcodeMapFXXX.Add(0x18, loadSoundTimer);
+            _opcodeMapFXXX.Add(0x1E, addToIndex);
+            _opcodeMapFXXX.Add(0x29, addressFontCharacter);
+            _opcodeMapFXXX.Add(0x33, storeBCD);
+            _opcodeMapFXXX.Add(0x65, fillRegisters);
 
             running = false;
             paused = false;
@@ -93,6 +245,32 @@ namespace Chip8
 
         #endregion
 
+        #region Accessible Properties
+
+        internal byte[] Screen
+        {
+            get { lock (_sync) { return (_screen); } }
+            set { lock (_sync) { _screen = value; } }
+        }
+
+        #endregion
+
+        #region Accessible Callbacks
+
+        internal Action ScreenRefreshed
+        {
+            get { lock (_sync) { return (_screenRefreshed); } }
+            set { lock (_sync) { _screenRefreshed = value; } }
+        }
+
+        internal Func<byte> GetKeypress
+        {
+            get { lock (_sync) { return (_getKeypress); } }
+            set { lock (_sync) { _getKeypress = value; } }
+        }
+
+        #endregion
+
         #region Accessible Methods
 
         internal void Initialize()
@@ -100,9 +278,14 @@ namespace Chip8
             Array.Clear(_memory, 0, _memory.Length);
             Array.Clear(_v, 0, _v.Length);
             Array.Clear(_stack, 0, _stack.Length);
-            Array.Clear(_screen, 0, _screen.Length);
+            Array.Clear(Screen, 0, Screen.Length);
 
-            byte[] rom = File.ReadAllBytes("game.ch8");
+            byte[] rom = File.ReadAllBytes("ROMs\\Chip8 emulator Logo [Garstyciuks].ch8");
+            //byte[] rom = File.ReadAllBytes("ROMs\\Chip8 Picture.ch8");
+            //byte[] rom = File.ReadAllBytes("ROMs\\Breakout [Carmelo Cortez, 1979].ch8");
+            //byte[] rom = File.ReadAllBytes("ROMs\\15 Puzzle[Roger Ivie].ch8");
+            //byte[] rom = File.ReadAllBytes("ROMs\\Cave.ch8");
+
             Buffer.BlockCopy(rom, 0, _memory, (int)MEMORY_ROM_OFFSET, rom.Length);
 
             _i = 0;
@@ -133,58 +316,63 @@ namespace Chip8
         {
             while (running)
             {
-                if (paused)
-                    Thread.Sleep(10);
-                else
+                if (!paused)
                     clock();
             }
         }
 
         private void clock()
         {
+            //Thread.Sleep(1);
+
             uint opcode = (uint)(_memory[_pc] << 8) | _memory[_pc + 1];
             _pc += OPCODE_SIZE;
 
             byte opcodeMSN = (byte)((opcode & 0xF000) >> 12);
 
-            if (!_opcodeMap.ContainsKey(opcodeMSN))
-                Debug.WriteLine("Not Implemented");
-            else
-                _opcodeMap[opcodeMSN](opcode);
+            Debug.Assert(_opcodeMap.ContainsKey(opcodeMSN), string.Format("Opcode 0x{0} not implemented", opcode.ToString("X4")));
+
+            _opcodeMap[opcodeMSN](opcode);
         }
 
         #endregion
 
         #region Operations Mapping
 
+        private void map00EXOperations(uint opcode)
+        {
+            byte opcodeLSB = (byte)(opcode & 0x00FF);
+
+            Debug.Assert(_opcodeMap00EX.ContainsKey(opcodeLSB), string.Format("Opcode 0x{0} not implemented", opcode.ToString("X4")));
+
+            _opcodeMap00EX[opcodeLSB](opcode);
+        }
+
         private void map8XXXOperations(uint opcode)
         {
             byte opcodeLSN = (byte)(opcode & 0x000F);
 
-            if (!_opcodeMap8XXX.ContainsKey(opcodeLSN))
-                Debug.WriteLine("Not Implemented");
-            else
-                _opcodeMap8XXX[opcodeLSN](opcode);
+            Debug.Assert(_opcodeMap8XXX.ContainsKey(opcodeLSN), string.Format("Opcode 0x{0} not implemented", opcode.ToString("X4")));
+
+            _opcodeMap8XXX[opcodeLSN](opcode);
         }
 
         private void mapEXXXOperations(uint opcode)
         {
             byte opcodeLSB = (byte)(opcode & 0x00FF);
 
-            if (!_opcodeMapEXXX.ContainsKey(opcodeLSB))
-                Debug.WriteLine("Not Implemented");
-            else
-                _opcodeMapEXXX[opcodeLSB](opcode);
+            Debug.Assert(_opcodeMapEXXX.ContainsKey(opcodeLSB), string.Format("Opcode 0x{0} not implemented", opcode.ToString("X4")));
+
+            _opcodeMapEXXX[opcodeLSB](opcode);
         }
 
         private void mapFXXXOperations(uint opcode)
         {
             byte opcodeLSB = (byte)(opcode & 0x00FF);
 
-            if (!_opcodeMapFXXX.ContainsKey(opcodeLSB))
-                Debug.WriteLine("Not Implemented");
-            else
-                _opcodeMapFXXX[opcodeLSB](opcode);
+            Debug.Assert(_opcodeMapFXXX.ContainsKey(opcodeLSB), string.Format("Opcode 0x{0} not implemented", opcode.ToString("X4")));
+
+            _opcodeMapFXXX[opcodeLSB](opcode);
         }
 
         #endregion
@@ -195,6 +383,15 @@ namespace Chip8
         private void jump(uint opcode)
         {
             uint address = opcode & 0x0FFF;
+            _pc = address;
+        }
+
+        // 2NNN
+        private void callSubroutine(uint opcode)
+        {
+            uint address = opcode & 0x0FFF;
+            _stack[_sp] = _pc;
+            _sp++;
             _pc = address;
         }
 
@@ -233,7 +430,7 @@ namespace Chip8
         }
 
         // ANNN
-        private void loadIRegister(uint opcode)
+        private void loadIndex(uint opcode)
         {
             uint address = opcode & 0x0FFF;
             _i = address;
@@ -254,12 +451,17 @@ namespace Chip8
         // DXYN
         private void drawSprite(uint opcode)
         {
-            byte xLocation = (byte)((opcode & 0x0F00) >> 8);
-            byte yLocation = (byte)((opcode & 0x00F0) >> 4);
+            byte xRegister = (byte)((opcode & 0x0F00) >> 8);
+            byte yRegister = (byte)((opcode & 0x00F0) >> 4);
             byte numberOfSpriteLines = (byte)(opcode & 0x000F);
+
+            byte xLocation = (byte)(_v[xRegister] & 0x3F);
+            byte yLocation = (byte)(_v[yRegister] & 0x1F);
 
             byte[] sprite = new byte[numberOfSpriteLines];
             Buffer.BlockCopy(_memory, (int)_i, sprite, 0, numberOfSpriteLines);
+
+            bool collision = false;
 
             for (int spriteLineIndex = 0; spriteLineIndex < numberOfSpriteLines; spriteLineIndex++)
             {
@@ -267,7 +469,17 @@ namespace Chip8
 
                 while (pixelLocation != 0x00)
                 {
-                    _screen[(yLocation * SCREEN_WIDTH) + xLocation] = (sprite[spriteLineIndex] & pixelLocation) == 0 ? (byte)0 : (byte)1;
+                    if ((sprite[spriteLineIndex] & pixelLocation) != 0)
+                    {
+                        if (Screen[(yLocation * SCREEN_WIDTH) + xLocation] == 0)
+                            Screen[(yLocation * SCREEN_WIDTH) + xLocation] = 1;
+                        else
+                        {
+                            Screen[(yLocation * SCREEN_WIDTH) + xLocation] = 0;
+                            collision = true;
+                        }
+                    }
+
                     pixelLocation = (byte)(pixelLocation >> 1);
                     xLocation++;
 
@@ -275,17 +487,46 @@ namespace Chip8
                         break;
                 }
 
+                xLocation = _v[xRegister];
                 yLocation++;
 
                 if (yLocation >= SCREEN_HEIGHT)
                     break;
             }
 
-            if (NewDrawing != null)
-                NewDrawing(_screen);
+            _v[0xF] = collision ? (byte)1 : (byte)0;
+
+            //lock (_sync)
+            //{
+            //    if (_screenRefreshed != null)
+            //        _screenRefreshed();
+            //}
 
             //Thread.Sleep(60000);
         }
+
+        #region 00EX Operations
+
+        // 00E0
+        private void clearScreen(uint opcode)
+        {
+            Array.Clear(Screen, 0, Screen.Length);
+
+            //lock (_sync)
+            //{
+            //    if (_screenRefreshed != null)
+            //        _screenRefreshed();
+            //}
+        }
+
+        // 00EE
+        private void returnSubroutine(uint opcode)
+        {
+            _sp--;
+            _pc = _stack[_sp];
+        }
+
+        #endregion
 
         #region 8XXX Operations
 
@@ -312,10 +553,38 @@ namespace Chip8
 
         #region EXXX Operations
 
+        // EX9E
+        private void jumpIfKeyPressed(uint opcode)
+        {
+            byte registerX = (byte)((opcode & 0x0F00) >> 8);
+
+            byte keypress = 0xFF;
+            lock (_sync)
+            {
+                Debug.Assert(_getKeypress != null, "The GetKeypress callback is not properly set");
+
+                keypress = _getKeypress();
+            }
+
+            if (_v[registerX] == keypress)
+                _pc += OPCODE_SIZE;
+        }
+
         // EXA1
         private void jumpIfKeyNotPressed(uint opcode)
         {
-            // need to implement
+            byte registerX = (byte)((opcode & 0x0F00) >> 8);
+
+            byte keypress = 0;
+            lock (_sync)
+            {
+                Debug.Assert(_getKeypress != null, "The GetKeypress callback is not properly set");
+
+                keypress = _getKeypress();
+            }
+
+            if (_v[registerX] != keypress)
+                _pc += OPCODE_SIZE;
         }
 
         #endregion
@@ -325,22 +594,63 @@ namespace Chip8
         // FX0A
         private void waitForKeypress(uint opcode)
         {
-            byte[] temp2 = new byte[1];
+            byte registerX = (byte)((opcode & 0x0F00) >> 8);
 
-            while (temp2[0] == 0)
+            byte keypress = 0;
+
+            while ((keypress == 0) && running)
             {
-                int numkeys = 0;
-                IntPtr temp = SDL.SDL_GetKeyboardState(out numkeys);
+                lock (_sync)
+                {
+                    Debug.Assert(_getKeypress != null, "The GetKeypress callback is not properly set");
 
-
-
-                Marshal.Copy(temp, temp2, 0, 1);
+                    keypress = _getKeypress();
+                }
             }
-            //GCHandle handle = (GCHandle)temp;
-            //byte temp2 = (handle.Target as byte);
-            //List<string> list2 = (handle2.Target as List<string>);
-            //list2.Add("hello world");
-            // need to implement
+
+            _v[registerX] = keypress;
+        }
+
+        // FX18
+        private void loadSoundTimer(uint opcode)
+        {
+            byte registerX = (byte)((opcode & 0x0F00) >> 8);
+            _soundTimer = _v[registerX];
+        }
+
+        // FX1E
+        private void addToIndex(uint opcode)
+        {
+            byte registerX = (byte)((opcode & 0x0F00) >> 8);
+            _i += _v[registerX];
+        }
+
+        // FX29
+        private void addressFontCharacter(uint opcode)
+        {
+            byte registerX = (byte)((opcode & 0x0F00) >> 8);
+            _i = (byte)(_v[registerX] * FONT_SIZE);
+        }
+
+        // FX33
+        private void storeBCD(uint opcode)
+        {
+            byte registerX = (byte)((opcode & 0x0F00) >> 8);
+
+            _memory[_i] = (byte)((_v[registerX] / 100) % 10);
+            _memory[_i + 1] = (byte)((_v[registerX] / 10) % 10);
+            _memory[_i + 2] = (byte)(_v[registerX] % 10);
+        }
+
+        // FX65
+        private void fillRegisters(uint opcode)
+        {
+            byte endRegister = (byte)((opcode & 0x0F00) >> 8);
+
+            for (int register = 0; register <= endRegister; register++)
+            {
+                _v[register] = _memory[_i + register];
+            }
         }
 
         #endregion
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,99 +1,124 @@
-﻿using SDL2;
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
 using System;
-using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Timers;
 using System.Windows;
-using System.Windows.Interop;
+using System.Windows.Forms;
 using System.Windows.Threading;
 
 namespace Chip8
 {
     public partial class MainWindow : Window
     {
-        SDL.SDL_Rect _drawableRegion;
-        IntPtr _sdlRendererPointer;
-        IntPtr _sdlSurfacePointer;
-        SDL.SDL_Surface _sdlSurface;
-        IntPtr _sdlTexturePointer;
-
-        Engine _engine;
+        private GLControl _openGLControl;
+        private Engine _engine;
+        private System.Timers.Timer _screenRefreshTimer;
 
         public MainWindow()
         {
             InitializeComponent();
+
+            _openGLControl = new GLControl();
+            _openGLControl.Width = (int)openGLControlHost.Width;
+            _openGLControl.Height = (int)openGLControlHost.Height;
+            openGLControlHost.Child = _openGLControl;
+            _openGLControl.Load += _openGLControl_Load;
+            _openGLControl.Paint += _openGLControl_Paint;
         }
 
-        private void drawScreen(byte[] screen)
+        private void _openGLControl_Load(object sender, EventArgs e)
+        {
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+
+            GL.Ortho(0, _openGLControl.Width, _openGLControl.Height, 0, -1, 1);
+            GL.Viewport(0, 0, _openGLControl.Width, _openGLControl.Height);
+        }
+
+        public void _openGLControl_Paint(object sender, PaintEventArgs e)
+        {
+            GL.ClearColor(Color.Black);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+
+            GL.Color3(Color.White);
+
+            GL.Begin(PrimitiveType.Points);
+
+            byte[] screen = _engine.Screen;
+            for (int screenY = 0; screenY < Engine.SCREEN_HEIGHT; screenY++)
+            {
+                for (int screenX = 0; screenX < Engine.SCREEN_WIDTH; screenX++)
+                {
+                    if (screen[(screenY * Engine.SCREEN_WIDTH) + screenX] == 1)
+                    {
+                        GL.Vertex2(screenX, screenY);
+                    }
+                }
+            }
+
+            GL.End();
+
+            _openGLControl.SwapBuffers();
+        }
+
+        private void invalidateScreen()
         {
             Dispatcher.BeginInvoke((Action)(() =>
             {
-                for (int screenY = 0; screenY < 32; screenY++)
-                {
-                    for (int screenX = 0; screenX < 64; screenX++)
-                    {
-                        if (screen[(screenY * 64) + screenX] == 1)
-                        {
-                            SDL.SDL_Rect drawRect = new SDL.SDL_Rect()
-                            {
-                                x = screenX,
-                                y = screenY,
-                                w = 1,
-                                h = 1
-                            };
-                            SDL.SDL_FillRect(_sdlSurfacePointer, ref drawRect, SDL.SDL_MapRGB(_sdlSurface.format, 0xFF, 0xFF, 0xFF));
-                        }
-                    }
-                }
-
-                SDL.SDL_UpdateTexture(_sdlTexturePointer, IntPtr.Zero, _sdlSurface.pixels, _sdlSurface.pitch);
-
-                SDL.SDL_RenderClear(_sdlRendererPointer);
-                SDL.SDL_RenderCopy(_sdlRendererPointer, _sdlTexturePointer, IntPtr.Zero, ref _drawableRegion);
-                SDL.SDL_RenderPresent(_sdlRendererPointer);
+                _openGLControl.Invalidate();
             }));
+        }
+
+        private byte getKeypress()
+        {
+            Dictionary<Key, byte> keyValueMap = new Dictionary<Key, byte>()
+            {
+                { Key.Number0, 0x0 }, { Key.Number1, 0x1 }, { Key.Number2, 0x2 }, { Key.Number3, 0x3 },
+                { Key.Number4, 0x4 }, { Key.Number5, 0x5 }, { Key.Number6, 0x6 }, { Key.Number7, 0x7 },
+                { Key.Number8, 0x8 }, { Key.Number9, 0x9 }, { Key.A, 0xA }, { Key.B, 0xB },
+                { Key.C, 0xC }, { Key.D, 0xD }, { Key.E, 0xE }, { Key.F, 0xF }
+            };
+
+            KeyboardState keyboardState = Keyboard.GetState();
+
+            foreach (Key key in keyValueMap.Keys)
+                if (keyboardState.IsKeyDown(key))
+                    return (keyValueMap[key]);
+            
+            return (0xFF);
         }
 
         private void Window_Activated(object sender, EventArgs e)
         {
             if (_engine == null)
             {
-                _drawableRegion = new SDL.SDL_Rect()
-                {
-                    x = (int)drawableSurface.TransformToAncestor(Application.Current.MainWindow).Transform(new Point(0, 0)).X,
-                    y = (int)drawableSurface.TransformToAncestor(Application.Current.MainWindow).Transform(new Point(0, 0)).Y,
-                    w = (int)drawableSurface.Width,
-                    h = (int)drawableSurface.Height
-                };
-                SDL.SDL_Rect drawableSize = new SDL.SDL_Rect() { w = _drawableRegion.w, h = _drawableRegion.h };
-
-                SDL.SDL_Init(SDL.SDL_INIT_EVERYTHING);
-
-                //IntPtr sdlWindowPointer = SDL.SDL_CreateWindow("My Game Window", SDL.SDL_WINDOWPOS_UNDEFINED, SDL.SDL_WINDOWPOS_UNDEFINED, 256, 128, SDL.SDL_WindowFlags.SDL_WINDOW_OPENGL);
-                IntPtr sdlWindowPointer = SDL.SDL_CreateWindowFrom(new WindowInteropHelper(Application.Current.MainWindow).Handle);
-                _sdlRendererPointer = SDL.SDL_CreateRenderer(sdlWindowPointer, -1, 0);
-                SDL.SDL_SetRenderDrawColor(_sdlRendererPointer, 0xFF, 0xFF, 0xFF, 0xFF);
-
-                _sdlSurfacePointer = SDL.SDL_CreateRGBSurface(0, drawableSize.w, drawableSize.h, 32, 0x00FF0000, 0x0000FF00, 0x000000FF, 0xFF000000);
-                _sdlSurface = (SDL.SDL_Surface)Marshal.PtrToStructure(_sdlSurfacePointer, typeof(SDL.SDL_Surface));
-
-                _sdlTexturePointer = SDL.SDL_CreateTexture(_sdlRendererPointer, SDL.SDL_PIXELFORMAT_ARGB8888, (int)SDL.SDL_TextureAccess.SDL_TEXTUREACCESS_STREAMING, drawableSize.w, drawableSize.h);
-
-                SDL.SDL_FillRect(_sdlSurfacePointer, ref drawableSize, SDL.SDL_MapRGB(_sdlSurface.format, 0x00, 0x00, 0x00));
-
-                SDL.SDL_UpdateTexture(_sdlTexturePointer, IntPtr.Zero, _sdlSurface.pixels, _sdlSurface.pitch);
-
-                SDL.SDL_RenderClear(_sdlRendererPointer);
-                SDL.SDL_RenderCopy(_sdlRendererPointer, _sdlTexturePointer, IntPtr.Zero, ref _drawableRegion);
-                SDL.SDL_RenderPresent(_sdlRendererPointer);
+                _screenRefreshTimer = new System.Timers.Timer(64);
+                _screenRefreshTimer.Elapsed += _screenRefreshTimer_Elapsed;
+                _screenRefreshTimer.Start();
 
                 _engine = new Engine();
+                _engine.ScreenRefreshed = invalidateScreen;
+                _engine.GetKeypress = getKeypress;
                 _engine.Initialize();
-                _engine.NewDrawing = drawScreen;
                 _engine.Start();
             }
         }
 
+        private void _screenRefreshTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            _openGLControl.Invalidate();
+        }
+
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
+            _screenRefreshTimer.Stop();
             _engine.Stop();
         }
     }
